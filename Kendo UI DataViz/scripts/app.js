7782b8025e4acd66fac65c7d96bb8dc4e42a890a
Tyanko Yordanov <tjordanov@gmail.com>
2013-10-30 04:10:58 -0500

    Workspace created.

---
+++ Kendo UI DataViz/scripts/app.js
@@ -0,0 +1,29 @@
+(function (global) {
+    var application,
+        mobileSkin = "",
+        defaultChartTheme = 'silver',
+        app = global.app = global.app || {};
+    
+    app.chartsTheme = defaultChartTheme;    
+
+    document.addEventListener("deviceready", function () {
+        application = new kendo.mobile.Application(document.body, { transition: "", layout: "mobile-tabstrip" });
+    }, false);
+
+    //Skin change function is for the demo. On real project only one theme should be chosen.
+    app.changeSkin = function (e) {
+        if (e.sender.element.text() === "Flat") {
+            e.sender.element.text("Native");
+            global.app.chartsTheme = 'flat';
+            mobileSkin = "flat";
+        }
+        else {
+            e.sender.element.text("Flat");
+            global.app.chartsTheme = defaultChartTheme;
+            mobileSkin = "";
+        }
+
+        application.skin(mobileSkin);
+        application.view().show();
+    }; 
+})(window);
